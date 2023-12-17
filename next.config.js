1d8035d43f6cc11823fbc2823703228ab0afe115
Kasper Petersen <krmp1991@gmail.com>
2023-12-17 05:26:32 +0100

    setup-1

--- next.config.js
+++ next.config.js
@@ -1,8 +1,32 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 
-module.exports = {
+const nextConfig = {
+  /* config options here */
+};
+
+module.exports = nextConfig;
+
+/*
+const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
   },
 };
+
+module.exports = nextConfig; /*
+
+/*export const reactStrictMode = true;
+export const experimental = {
+  appDir: true,
+};
+
+/*module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+};
+*/
