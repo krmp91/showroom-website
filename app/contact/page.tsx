83fdbec9324b0357c3b9788dee3c8a42aaed2cca
Kasper Petersen <krmp1991@gmail.com>
2023-12-20 11:15:02 +0100

    stay-tuned-add

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,14 +6,14 @@ import { ResearchGate } from "../icons/researchgate";
 
 const Contact = () => {
   return (
-    <div className="text-darkOrange text-center p-6">
-      <h1 className="text-2xl font-bold mb-4">CONTACT ME</h1>
-      <div className="flex justify-end">
+    <div>
+      <div>Contact</div>
+      {/*<div className="flex justify-end">
         <Mail className="fill-darkOrange h-6 w-6" />
         <Instagram className="fill-darkOrange h-5 w-5" />
         <Linkedin className="fill-darkOrange h-5 w-5" />
         <ResearchGate className="fill-darkOrange h-5 w-5" />
-      </div>
+  </div>*/}
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./global.css";
 import { Footer } from "./components/footer";
 import { Header } from "./components/header";
+import Image from "next/image";
 
 export const metadata = {
   title: "Next.js",
@@ -17,7 +18,22 @@ export default function RootLayout({
       <head />
       <body className="bg-roseGray">
         <Header />
-        <main>{children}</main>
+        <main>
+          <div className="flex flex-col items-center">
+            <h1 className="text-darkOrange">
+              CONTENT WILL COME SOON - Stay tuned!
+            </h1>
+            <div>
+              <Image
+                src="/images/content-coming-soon-stay-tuned-projects.jpg"
+                alt="design workspace"
+                width={949}
+                height={595}
+              />
+            </div>
+          </div>
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
