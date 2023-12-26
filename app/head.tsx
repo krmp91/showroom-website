fb1b460b557f8236e31cf2ce79eb7c050727e0c8
krmp91 <90442900+krmp91@users.noreply.github.com>
2023-12-26 19:46:14 +0100

    changes-api-stuff

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title></title>
+      <title>Louise Ravnløkke</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
-/* eslint-disable react/no-unescaped-entities */
 import React from "react";
-<style>
-  @import
-  url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap');
-</style>;
 
 const HomePage = () => {
   return <div>HomePage</div>;
